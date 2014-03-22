2b96df17f617ab8c4df5ad9d56736d542d7c2925
Danick Fort <danick.fort@he-arc.ch>
2014-03-22 15:33:19 +0100

    Added gamePhysics missing ifle, OOPS!

---
+++ ChickenPanic/Core/GamePhysics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChickenPanic.Core
+{
+    class GamePhysics
+    {
+        private double verticalGravity = 1;
+        private double velocity = 1;
+        private double currentHeight;
+
+        public const double TerminalVelocity = 5;
+
+        public double VerticalGravity
+        {
+            get { return verticalGravity; }
+            set { verticalGravity = value; }
+        }
+
+        public double Velocity
+        {
+            get { return velocity; }
+            set { velocity = value;  }
+        }
+        public double CurrentHeight
+        {
+            get { return currentHeight; }
+            set { currentHeight = value; }
+        }
+    }
+}
